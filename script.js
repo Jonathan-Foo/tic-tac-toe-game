50c4f37264feadcb222d2add9b20cbe6c9f37110
Jonathan Foo <Johnathan.foo@gmail.com>
2022-03-12 12:51:30 +1100

    Implemented logic to find game winner

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const logo = document.querySelector('.logo')
 const pvpModal = document.querySelector('.pvp-modal')
 const pveModal = document.querySelector('.pve-modal')
 const buttonContainer = document.querySelector('.button-container')
+const gameScreen = document.querySelector('.game-screen')
+
 
 pvpBtn.addEventListener('click', () => {
     startScreenTransition()
@@ -22,20 +24,52 @@ function startScreenTransition() {
 }
 
 // PVP FORM
-const pvpModalInputs = [...pvpModal.querySelectorAll('input')];
+
+const beginGameBtn = pvpModal.querySelector('.pvp-begin-btn')
+
+beginGameBtn.addEventListener('click', () => beginPvPGame())
+
+const pvpForm = document.querySelector('.pvp-form')
+const pvpFormInputs = [...pvpForm.querySelectorAll('input')];
+let playerOne = {}
+let playerTwo = {}
 
 
+function beginPvPGame() {
+    if (pvpFormInputs.every(input => input.validity.valid !== false)) {
+        pvpModal.classList.toggle('active')
+        gameScreen.classList.toggle('active')
+        playerOneCreator(pvpFormInputs[0].value)
+        playerTwoCreator(pvpFormInputs[1].value)
+    } else {
+        pvpForm.reportValidity()
+        return 
+    }
+}
+
+const playerOneCreator = (name) => {playerOne = {name: name, playerNumber: 'one', symbol: 'X'}}
+
+const playerTwoCreator = (name) => {playerTwo = {name: name, playerNumber: 'two', symbol: 'O'}}
 
 
+//PVE FORM 
+const beginPvEGameBtn = pveModal.querySelector('.pve-begin-btn')
 
+beginPvEGameBtn.addEventListener('click', () => beginPvEGame())
 
+function beginPvEGame() {
+    const pveForm = document.querySelector('.pve-form')
+    const pveFormInput = pveForm.querySelector('input');
 
-
-
-
-
-
-
+    if (pveFormInput.validity.valid !== false) {
+        pvpModal.classList.toggle('active')
+        gameScreen.classList.toggle('active')
+        playerOneCreator(pvpFormInputs[0].value)
+    } else {
+        pveForm.reportValidity()
+        return 
+    }
+    }
 
 
 
@@ -47,27 +81,44 @@ const gameBoard = [['','', ''],
                 ['', '', '']] 
 
 
-
 // WINNER CHECK
 function checkWinner() {
-    verticalWinCheck() 
-    horizontalWinCheck()
-    diagonalWinCheck()
+        verticalWinCheck() 
+        horizontalWinCheck()
+        diagonalWinCheck()
 }
 
 function verticalWinCheck() {
-    gameBoard.every(array => array[0] == gameBoard[0][0] )
-    gameBoard.every(array => array[1] == gameBoard[0][1] )
-    gameBoard.every(array => array[2] == gameBoard[0][2] )
+    let leftColumnWin = gameBoard[0][0] !== '' && gameBoard.every(array => array[0] == gameBoard[0][0]) 
+    let midColumnWin = gameBoard[0][1] !== '' && gameBoard.every(array => array[1] == gameBoard[0][1])
+    let rightColumnWin = gameBoard[0][2] !== '' && gameBoard.every(array => array[2] == gameBoard[0][2])
+    
+    if (leftColumnWin || midColumnWin || rightColumnWin) {
+        console.log('vertical win')
+    } else {
+        return 
+    }
 } 
 
 function horizontalWinCheck() {
-    gameBoard.map(array => array[0] == array[1] && array[1] == array[2])
+    let topRowWin = gameBoard[0][0] !== '' && gameBoard[0][0] == gameBoard[0][1] && gameBoard[0][1] == gameBoard[0][2]
+    let midRowWin = gameBoard[0][0] !== '' && gameBoard[0][0] == gameBoard[0][1] && gameBoard[0][1] == gameBoard[0][2]
+    let bottomRowWin = gameBoard[0][0] !== '' && gameBoard[0][0] == gameBoard[0][1] && gameBoard[0][1] == gameBoard[0][2]
+    if (topRowWin || midRowWin || bottomRowWin) {
+        console.log("horizontal win")
+    } else {
+        return 
+    }
 }
 
 function diagonalWinCheck() {
-    gameBoard[0][0] == gameBoard[1][1] && gameBoard[1][1] == gameBoard[2][2] 
-    gameBoard[0][2] == gameBoard[1][1] && gameBoard[1][1] == gameBoard[2][0] 
+    let diagonalOne = gameBoard[0][0] !== '' && gameBoard[0][0] == gameBoard[1][1] && gameBoard[1][1] == gameBoard[2][2] 
+    let diagonalTwo = gameBoard[0][2] !== '' && gameBoard[0][2] == gameBoard[1][1] && gameBoard[1][1] == gameBoard[2][0] 
+    if (diagonalOne || diagonalTwo) {
+        console.log('diagonal win')
+    } else {
+        return
+    }
 }
 
 
@@ -75,14 +126,21 @@ function diagonalWinCheck() {
 // CLICKING ON TITLE TRIGGER A SYMBOL TO BE DISPLAYED 
 let currentSymbol = 'X'
 const tiles = [...document.querySelectorAll('.tile')]
+// let turns = 0
 // CONNECT CLICK TO GAMEBOARD ARRAY
 tiles.forEach(tile => tile.addEventListener('click', () => {
     const indexA = tile.dataset.indexa
     const indexB = tile.dataset.indexb
-    tile.innerHTML = currentSymbol
-    gameBoard[indexA][indexB] = tile.innerHTML
-    console.log(gameBoard)
-    switchSymbol(currentSymbol)
+    if (gameBoard[indexA][indexB] == '') {
+        tile.innerHTML = currentSymbol
+        gameBoard[indexA][indexB] = tile.innerHTML
+        switchSymbol(currentSymbol)
+       
+        checkWinner()
+    } else {
+        return
+    }
+
 }))
 
 
